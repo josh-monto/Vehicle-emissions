70ab74939755ca7dd39fe64bacca5ef346ed7807
jjstapes <joshstapley_89@yahoo.com>
2022-04-09 15:25:52 -0600

    Update emission-data.py

--- emission-data.py
+++ emission-data.py
@@ -1,4 +1,5 @@
-# Import SDK packages
+#Code responsible for importing vehicle data, publishing all CO2 data to topic,
+#and subscribing to appropriate topics to receive max CO2 value from lambda function
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import time
 import json
